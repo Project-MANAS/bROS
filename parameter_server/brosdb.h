a7469f9752973649b77726aaed3649da98b8c9cb
vdasu <vishnu98dasu@gmail.com>
2018-09-26 22:30:57 +0530

    Add parameter_server

---
+++ parameter_server/brosdb.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef BROS_PARAMETER_SERVER_BROSDB_
+#define BROS_PARAMETER_SERVER_BROSDB_
+
+#include "hiredis/hiredis.h"
+
+namespace brosdb {
+
+    template<typename T>
+    void get(std::string key, T *value){
+        redisContext *context = redisConnect("127.0.0.1", 6379);
+        redisReply *reply = (redisReply*)redisCommand(context, "GET %b", key.c_str(), key.length());
+        memcpy(value, reply->str, sizeof(T));
+        std::cout<<"size"<<" "<<sizeof(T)<<" "<<typeid(T).name()<<"\n";
+        freeReplyObject(reply);
+    }
+
+    template<typename T>
+    void set(std::string key, T value){
+        redisContext *context = redisConnect("127.0.0.1", 6379);
+        redisReply *reply = (redisReply*)redisCommand(context, "SET %b %b", key.c_str(), key.length(), &value, sizeof(T));
+        freeReplyObject(reply);
+    }
+}
+
+#endif
